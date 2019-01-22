7b9cef6207cce29e645b9f28cbe9a162a605adb1
Ahmed Selim Üzüm <ahmedselimuzum@gmail.com>
2019-01-22 17:08:52 +0300

    Hata Düzeltmeleri

--- 2016/pizzalar/Solution.java
+++ 2016/pizzalar/Solution.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Main {
 
 
-    public static void main(String[] args) {
+    public static void Solution(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
 
         ArrayList<Integer> dizi = oku();
